f93d17cc9062e31a2a2cce5295534f3bd4d2e2c3
Moeez Tahir <42956596+moeeztaher@users.noreply.github.com>
2024-08-27 08:16:39 +0200

    Update install_prometheus.sh

--- prometheus/install_prometheus.sh
+++ prometheus/install_prometheus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Download and install Prometheus
 curl -LO https://github.com/prometheus/prometheus/releases/download/v2.47.0/prometheus-2.47.0.linux-amd64.tar.gz
 tar xvf prometheus-2.47.0.linux-amd64.tar.gz
 sudo mv prometheus-2.47.0.linux-amd64/prometheus /usr/local/bin/
